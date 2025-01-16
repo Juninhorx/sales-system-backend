db1e747831465f2a0c2b02360bfaf64ac844cb64
Ricardo Xavier <rxc.projects@gmail.com>
2025-01-16 14:45:05 -0300

    OrderItem, many-to-many association with extra attributes

--- src/main/java/com/ricardoxavier/course/config/TestConfig.java
+++ src/main/java/com/ricardoxavier/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.ricardoxavier.course.entities.Category;
 import com.ricardoxavier.course.entities.Order;
+import com.ricardoxavier.course.entities.OrderItem;
 import com.ricardoxavier.course.entities.Product;
 import com.ricardoxavier.course.entities.User;
 import com.ricardoxavier.course.entities.enums.OrderStatus;
 import com.ricardoxavier.course.repositories.CategoryRepository;
+import com.ricardoxavier.course.repositories.OrderItemRepository;
 import com.ricardoxavier.course.repositories.OrderRepository;
 import com.ricardoxavier.course.repositories.ProductRepository;
 import com.ricardoxavier.course.repositories.UserRepository;
@@ -33,6 +35,9 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 
 	// Método da interface que roda sempre que a aplicação for executada;
 	@Override
@@ -70,6 +75,13 @@ public class TestConfig implements CommandLineRunner{
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
 	}
 
---
+++ src/main/java/com/ricardoxavier/course/repositories/OrderItemRepository.java
@@ -0,0 +1,9 @@
+package com.ricardoxavier.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ricardoxavier.course.entities.OrderItem;
+import com.ricardoxavier.course.entities.pk.OrderItemPK;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemPK>{
+}
